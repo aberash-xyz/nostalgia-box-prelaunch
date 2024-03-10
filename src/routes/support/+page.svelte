3f39ea3b7c5dbc9bd61095fc3d4ab1be6bb1658b
nati <natibekele@gmail.com>
2024-03-10 17:30:24 -0400

    add support route

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,40 @@
+<div class="container">
+	<article class="article-main">
+		<h1>Need some help?</h1>
+
+		<p>Reach out to us. <br /> <br />support at aberash.xyz</p>
+	</article>
+</div>
+
+<style>
+	.container {
+		padding-top: 10.4rem;
+		width: 100%;
+		height: auto;
+		font-family: 'Ostrich Sans';
+	}
+	.article-main {
+		padding: 2.4rem 3.6rem;
+		width: 100%;
+		max-width: 80rem;
+		margin: auto;
+	}
+	h1 {
+		font-size: 4.2rem;
+	}
+
+	h2 {
+		font-size: 2.8rem;
+	}
+
+	p,
+	li {
+		font-size: 1.8rem;
+	}
+
+	@media (max-width: 500px) {
+		.container {
+			padding-top: 12.4rem;
+		}
+	}
+</style>
